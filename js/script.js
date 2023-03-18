a5766916fa8709a9b9a66abf948394c5486cdaaf
JeanArce <113121403+JeanArce@users.noreply.github.com>
2023-03-18 10:40:15 +0100

    creating form using js

--- js/script.js
+++ js/script.js
@@ -24,6 +24,77 @@ document.addEventListener('click', function(event) {
     }
 });
  
-  
-  
+
+// below is for error validation in ser page form
+
+let nameEl = document.querySelector("#name");
+let nameError = document.getElementById("nameError");
+
+nameEl.addEventListener("input", function(event) {
+    if(nameEl.value.length > 0) {
+        nameError.style.display = 'none';
+    } else {
+        nameError.style.display = 'block';
+    }
+});
+
+
+let email = document.getElementById("email");
+let emailError = document.getElementById("emailError");
+
+email.addEventListener("input", function(event) {
+    if(validEmail(email.value)) {
+        emailError.style.display = 'none';
+    } else {
+        emailError.style.display = 'block';
+    }
+});
+
+let password = document.querySelector("#password");
+let passwordError = document.getElementById("passwordError");
+
+password.addEventListener("input", function(event) {
+    if(password.value.length >= 6) {
+        passwordError.style.display = 'none';
+    } else {
+        passwordError.style.display = 'block';
+    }
+});
+
+function validEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+
+function submitUserForm(event) {
+    event.preventDefault();
+   
+    if(nameEl.value.length == 0) {
+        nameError.style.display = 'block';
+    }
+
+    if(!validEmail(email.value)) {
+        emailError.style.display = 'block';
+    }
+
+    if(password.value.length < 6) {
+        passwordError.style.display = 'block';
+    }
+
+
+}
+
+
+function resetUserForm(event) {
+    event.preventDefault();
+    console.log('lets reset');
+    nameEl.value = "";
+    email.value = "";
+    password.value = "";
+    
+    nameError.style.display = "none";
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
+}
   
